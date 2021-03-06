5ab37d47884ebdd82dc62796dd74db7b3f58eb82
SilentDepth <silentdepth@outlook.com>
2021-03-07 00:38:10 +0800

    fix: notification was sent even if no update

--- main.ts
+++ main.ts
@@ -15,18 +15,20 @@ void async function main () {
   const updated = ([] as [string, [string, string]][]).concat(await checkAppVersion(), await checkDesktopVersion())
   const lastCheck = JSON.parse(fs.readFileSync('./last-check.json', 'utf-8')) as LastCheck
 
-  await fetch(process.env.TG_BOT_WEBHOOK_URL!, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify({
-      versions: Object.fromEntries(Object.entries(lastCheck).map(([type, latest]) => {
-        const found = updated.find(it => it[0] === type)
-        return [type, found?.[1] ?? (typeof latest === 'string' ? latest : latest.version)]
-      })),
-    }),
-  })
+  if (updated.length) {
+    await fetch(process.env.TG_BOT_WEBHOOK_URL!, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify({
+        versions: Object.fromEntries(Object.entries(lastCheck).map(([type, latest]) => {
+          const found = updated.find(it => it[0] === type)
+          return [type, found?.[1] ?? (typeof latest === 'string' ? latest : latest.version)]
+        })),
+      }),
+    })
+  }
 
   fs.writeFileSync('./UPDATED', updated.map(([type]) => {
     const data = lastCheck[type]
